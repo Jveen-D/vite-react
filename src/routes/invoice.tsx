4baba46eb0f85e3791019e754b3fc94b325949c4
杜文杰 <du.wenjie@bjblackhole.com>
2022-07-20 18:18:24 +0800

    chore: update

--- src/routes/invoice.tsx
+++ src/routes/invoice.tsx
@@ -1,11 +1,17 @@
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { getInvoice, deleteInvoice } from "../data";
-
+import { useEffect } from "react";
 export default function Invoice() {
   let navigate = useNavigate();
   let location = useLocation();
   let params: any = useParams();
   let invoice = getInvoice(parseInt(params.invoiceId, 10));
+  useEffect(() => {
+    console.log("🚀 ~ file: 执行内部事件");
+    return () => {
+      console.log("🚀 ~ file: 销毁组件");
+    };
+  }, []);
   return (
     <main style={{ padding: "1rem" }}>
       <h2>Total Due: {invoice.amount}</h2>
